bdff88526079767cc4f1907dfe1cf3a68571c408
vivekgith <vermavivekraj103@gmail.com>
2025-08-31 11:15:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print(5+6)
+print("I am vivek Raj Verma")
+print("What are yoy looking for?")
+print("what do you do for living?")
+print("Are you curious to learn python?")
+print("Am I doing my programme practise?")
+print("What do you think about your career?") 
+
+
